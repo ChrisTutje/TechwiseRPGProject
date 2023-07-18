67510371aa577921385af453a1620c327f42aca9
Flaremancer1 <109772688+ChrisTutje@users.noreply.github.com>
2023-07-17 19:00:58 -0500

    created prefabs
    created more prefabs

--- TechwiseRPGProject/Assets/Enemies/battleScripts/Unit.cs
+++ TechwiseRPGProject/Assets/Enemies/battleScripts/Unit.cs
@@ -6,11 +6,31 @@ public class Unit : MonoBehaviour //variable fields for each unit
 {
     public string unitName;
     public int unitLevel;
-
-    public int damage;
+    //public int exp; 
 
     public int maxHp;
     public int currentHp;
+    //public int maxMp;
+    //public int currentMP;
+    //public int maxStamina;
+    //public int currentStamina;
+
+    public int attack;
+    //public int defence;
+    //public int accuracy;
+    //public int evasion;
+    //public int speed;
+    //public int resistance;
+    //public int critical; 
+    
+    //public int strength;
+    //public int perception;
+    //public int endurance;
+    //public int charisma;
+    //public int intellegence;
+    //public int agility;
+    //public int luck; 
+
 
     public bool TakeDamage(int dmg) {
         currentHp -= dmg;
