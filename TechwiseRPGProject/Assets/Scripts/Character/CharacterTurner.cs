bd888c2a35b6919cb7bde90d08f168f628de4f2d
adamahmetasevic <adamahmetasevic@gmail.com>
2023-07-27 20:52:01 -0500

    Start of Animations
    Created Side, Up, and Down Animations. Still have to script them into player

---
+++ TechwiseRPGProject/Assets/Scripts/Character/CharacterTurner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTurner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //17:29
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
