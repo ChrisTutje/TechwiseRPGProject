989faa626f5969e4b9f95e05ecb37256f62ebfed
Torirem03 <113369584+Torirem03@users.noreply.github.com>
2023-08-01 21:56:26 -0500

    cutscenes are finished
    forest scene opens and goes to burning village then to start of player game (this can easily be edited if you all want a different starting scene)

---
+++ TechwiseRPGProject/Assets/Cutscenes/C_Assets/UI text box/TextBoxManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public TextMeshProUGUI theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public Character player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Character>();
+        if(textFile != null){
+            textLines = (textFile.text.Split("\n"));
+        }
+
+        if(endAtLine == 0){
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    void Update()
+    {
+        theText.text = textLines[currentLine] + "\n" + textLines[currentLine +1] + "\n" + textLines[currentLine+2];
+
+
+        if(Input.GetKeyDown(KeyCode.Return)){
+            currentLine += 3;
+        }
+
+        if(currentLine > endAtLine){
+            //textBox.SetActive(false);
+            SceneManager.LoadScene("PlayerMovementtester");
+        }
+
+    }
+}
