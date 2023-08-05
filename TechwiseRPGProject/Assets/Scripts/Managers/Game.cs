01713960c0a9e0b153db30b7bd100a638cbe3458
adamahmetasevic <adamahmetasevic@gmail.com>
2023-08-04 22:02:23 -0500

    Created Manager Folder/Script
    With all the switching between battle and overworld, I am going to modularize the code by putting the script together

---
+++ TechwiseRPGProject/Assets/Scripts/Managers/Game.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { //19:23
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
