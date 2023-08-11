4341c51da033b2ab4163c094b6d406a657dec263
adamahmetasevic <adamahmetasevic@gmail.com>
2023-08-10 21:39:08 -0500

    Rewriting Game File
    Was going to use DontDestroyOnLoad(this) for saving position. Ran into issue of multiple player instances

--- TechwiseRPGProject/Assets/Scripts/Managers/Game.cs
+++ TechwiseRPGProject/Assets/Scripts/Managers/Game.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+public enum GameState
+{
+    Overworld,
+    Cutscene,
+    Battle,
+    Menu,
+}
 
 public class Game  
 {
     public Character character;
     public Vector2 playerPosition;
     
-    public Game(Character character)
-    {
-        this.character = character;
-    }
+   public static GameState State {get; private set;}
+   public static Map Map {get; private set; }
+   public static Player Player {get; private set; }
 
 
-
+    
    public void SwitchToSpecificScene()
     {
         float probability = .05f; // 10% chance
@@ -30,6 +35,7 @@ public class Game
         else if (SceneManager.GetSceneByName("BattleScene").isLoaded)
         {
             SceneManager.LoadScene("PlayerMovementtester");
+
         }
     }
 
